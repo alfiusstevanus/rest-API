00dd79d39a1248bcb2e882e4f03f21aee99cfdb5
alfiusstevanus <alfiusstevanus18@gmail.com>
2023-06-27 10:22:27 +0700

    berhasil mengambil data chanel dari api ytb

--- my-porfolio/index.php
+++ my-porfolio/index.php
@@ -1,3 +1,26 @@
+<?php
+function getCurl($url)
+{
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_URL, $url);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+  $result = curl_exec($curl);
+  curl_close($curl);
+  return json_decode($result, TRUE);
+}
+// channel info
+$result = getCurl('https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=UCnAoGsN8Wv25F1C-_lBy7jg&key=AIzaSyCEqfYn5Mi-BbuS0VK0oLjvhKGABcVE4Hw');
+
+$ytbProfile = $result['items'][0]['snippet']['thumbnails']['medium']['url'];
+$channelName = $result['items'][0]['snippet']['title'];
+$subscribers = $result['items'][0]['statistics']['subscriberCount'];
+
+//latest video
+$result = getCurl('https://www.googleapis.com/youtube/v3/search?key=AIzaSyCEqfYn5Mi-BbuS0VK0oLjvhKGABcVE4Hw&channelId=UCnAoGsN8Wv25F1C-_lBy7jg&maxResults=1&order=date&part=snippet');
+
+$latestVideoId = $result['items'][0]['id']['videoId'];
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -82,17 +105,43 @@
         <div class="col-md-5">
           <div class="row">
             <div class="col-md-4">
+              <img src="<?= $ytbProfile ?>" width="100" class="rounded-circle img-thumbnail">
+            </div>
+            <div class="col-md-8">
+              <h5><?= $channelName ?></h5>
+              <p><?= number_format($subscribers) ?> Subscribers</p>
+              <div class="g-ytsubscribe" data-channelid="UCnAoGsN8Wv25F1C-_lBy7jg" data-layout="default" data-count="default"></div>
+            </div>
+          </div>
+          <div class="row mt-3 pb-3">
+            <div class="col">
+              <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/<?= $latestVideoId ?>" frameborder="0" allowfullscreen></iframe>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-5">
+          <div class="row">
+            <div class="col-md-4">
               <img src="img/profile1.png" width="100" class="rounded-circle img-thumbnail">
             </div>
             <div class="col-md-8">
-              <h5>AlFi SteFa</h5>
-              <p>160 Subscribers</p>
+              <h5>@alfiusstevanus</h5>
+              <p>1500 Followers</p>
             </div>
           </div>
-          <div class="row">
+
+          <div class="row mt-3 pb-3">
             <div class="col">
-              <div class="embed-responsive embed-responsive-16by9">
-                <iframe class="embed-responsive-item" src="https://youtu.be/-BmTKA1xCm8" frameborder="0" allowfullscreen></iframe>
+              <div class="ig-thumbnail">
+                <img src="img/thumbs//1.png">
+              </div>
+              <div class="ig-thumbnail">
+                <img src="img/thumbs//2.png">
+              </div>
+              <div class="ig-thumbnail">
+                <img src="img/thumbs//3.png">
               </div>
             </div>
           </div>
@@ -250,6 +299,7 @@
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+  <script src="https://apis.google.com/js/platform.js"></script>
 </body>
 
 </html>
